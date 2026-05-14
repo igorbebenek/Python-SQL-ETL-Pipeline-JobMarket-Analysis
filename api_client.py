e76fe8e61f25aa4fe1af3058b2fc61c7991fccbf
Igor Bębenek <139233781+igorbebenek@users.noreply.github.com>
2026-05-14 11:19:25 +0200

    Added Power BI salary transparency and Top Companies dashboards

--- api_client.py
+++ api_client.py
@@ -9,9 +9,9 @@ base_url = "https://api.adzuna.com/v1/api/jobs"
 APP_KEY = os.getenv("APP_KEY")
 APP_ID = os.getenv("APP_ID")
 data = {"gb": [], "us": [], "pl": []}
-for page in range(1,201):
+for page in range(1,1000):
     for country in data:
-        url = f"{base_url}/{country}/search/{page}?app_id={APP_ID}&app_key={APP_KEY}&what=it"
+        url = f"{base_url}/{country}/search/{page}?app_id={APP_ID}&app_key={APP_KEY}"
         response = requests.get(url)
         if response.status_code == 200:
             json_content = response.json()
@@ -22,6 +22,6 @@ for page in range(1,201):
         time.sleep(1)
 
 print("Fetching finished. Now writing to file")
-with open("data/raw/raw_json_data_IT_only", 'w') as f:
+with open("data/raw/raw_json_data_2", 'w') as f:
     json.dump(data,f,indent=4)
 print("Finished")
